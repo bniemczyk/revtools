d02b61258c4970c36008442bebfd90832cf52344
Brandon Niemczyk <brandon.niemczyk@hp.com>
2012-08-23 12:37:49 -0500

    before replacing xrefs code with instruction walk

--- callgraph.py
+++ callgraph.py
@@ -6,7 +6,19 @@ class CallGraph(directed.DirectedGraph):
         import idautils
         import idc
         super(CallGraph, self).__init__()
+
         for f in idautils.Functions():
             self.add_node(idc.GetFunctionName(f))
             for cr in idautils.CodeRefsTo(f,0):
                 self.connect(idc.GetFunctionName(cr), idc.GetFunctionName(f))
+
+    def tag_distance(self, function_name):
+        import function
+        import idc
+        f = idc.LocByName(function_name)
+
+        for (n,l) in self.walk(function_name, direction='incoming'):
+            if l == 0:
+                continue
+
+            function.tag(idc.LocByName(n), 'distance %s' % (function_name), l)
